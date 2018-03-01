1732dcb686f9f1b2d3df390aec35d47b12efc898
tpalayda <taras.palayda@gmail.com>
2018-03-01 12:50:03 +0100

    t6

--- Exercises/Lista1/t6.cpp
+++ Exercises/Lista1/t6.cpp
@@ -1,22 +1,58 @@
 #include <iostream>
+#include <utility>
+#include <vector>
 
-struct MinMax
+std::pair<double,double> getMinMax(const std::vector<double> & vec)
 {
-    MinMax() : min(0), max(0) {}
-    int min;
-    int max;
-    MinMax getMinMax()
+    //min's 1st, max's 2nd
+    std::pair<double,double> min_max;
+    int i;
+    //if size is even then initialize the first two elements as minimum and maximum
+    if(!(vec.size() & 1))
     {
-        return MinMax();
+        if(vec[0] > vec[1])
+        {
+            min_max.first = vec[1];
+            min_max.second = vec[0];
+        }
+        else
+        {
+            min_max.first = vec[0];
+            min_max.second = vec[1];
+        }
+        i = 2;
     }
-};
-std::ostream& operator<<(std::ostream& stream, const MinMax& minmax)
-{
-    stream << minmax.min << "," << minmax.max << std::endl;
-    return stream;
+    else
+    {
+        min_max.first = vec[0];
+        min_max.second = vec[0];
+        i = 1;
+    }
+    while(i < vec.size()-1)
+    {
+        if(vec[i] > vec[i+1])
+        {
+            if(min_max.second < vec[i])
+                min_max.second = vec[i];
+            if(min_max.first > vec[i+1] )
+                min_max.first = vec[i+1];
+        }
+        else
+        {
+            if(min_max.second < vec[i+1])
+                min_max.second = vec[i+1];
+            if(min_max.first > vec[i])
+                min_max.first = vec[i];
+        }
+        i+=2;
+    }
+    return min_max;
 }
 int main()
 {
-    MinMax a = MinMax().getMinMax();
-    std::cout << a;
+    const std::vector<double> v {334,1231,230,231,545,691,321,31,5,10,5,4};
+    const auto minmax = getMinMax(v);
+    std::cout << "size: "  << v.size() 
+              << "\nmin: " << minmax.first
+              << "\nmax: " << minmax.second << std::endl;
 }
