a42f05b18e90ea658654e54e87b3666ea32bb44a
tpalayda <taras.palayda@gmail.com>
2018-03-06 19:21:59 +0100

    replacing unique pointers with raw + some modifications

--- Exercises/Lista1/t3.cpp
+++ Exercises/Lista1/t3.cpp
@@ -12,6 +12,6 @@ double polynomial(const std::vector<double> & v,const size_t & n,const double & 
 
 int main()
 {
-    std::vector<double> v {2,-6,2,-1};
+    std::vector<double> v {2,-6,2,-1}; // from n to n^0
     std::cout << polynomial(v,v.size(),3) << std::endl;
 }
