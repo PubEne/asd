01c942868eab22858fbb8d395890491f2e9d8c6f
tpalayda <taras.palayda@gmail.com>
2018-04-03 11:20:16 +0200

    t4 finished

--- Exercises/Lista4/t4.cpp
+++ Exercises/Lista4/t4.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+void printArray(int t[], int n)
+{
+    for(size_t i = 0; i < n; ++i)
+        std::cout << t[i] << " ";
+    std::cout << std::endl;
+}
 int partition(int t[],int l,int r)
 {
-    //if(l >= r) return l;
-    int x = t[r/2];
-    --l;
+    int x = t[(l+r)/2]; 
     for(;;)
     {
         while (t[--r] > x);
@@ -12,24 +16,23 @@ int partition(int t[],int l,int r)
         if (r > l)
             std::swap(t[r],t[l]);
         else
-            return l;
+            return r;
     }
-    return l;
 }
 int quick_select(int* t,int l,int r,const int& k)
 {
     if(l == r)
         return t[l];
-    int p = partition(t,l,r);
-    if(p < k)
-        return quick_select(t,p+1,r,k);
-    return quick_select(t,l,p,k);
+    int p = partition(t,l-1,r+1);
+    if(p > k)
+        return quick_select(t,l,p,k);
+    return quick_select(t,p+1,r,k);
 }
 int main()
 {
-    int t[] = {10,1,100,3,5};
-    const int n = sizeof(t)/sizeof(*t);
+    int a[] = {1,2,3,4,5,4,3,2,1};
+    const int n = sizeof(a)/sizeof(*a);
     for(size_t i = 0; i < n; ++i)
-        std::cout << quick_select(t,0,n,i) << " ";
+        std::cout << quick_select(a,0,n-1,i) << " ";
     std::cout << std::endl;
 }
