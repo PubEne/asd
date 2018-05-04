e71318cf632acda8e958d699d66441bb24183589
tpalayda <taras.palayda@gmail.com>
2018-05-04 13:39:36 +0200

    changed to class

--- Exercises/Lista7/t2.cpp
+++ Exercises/Lista7/t2.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
+#include <list>
+#include <vector>
 
 namespace ASD
 {
-    template<typename T>
-    struct node
+    template<typename T = int,size_t size=100>
+    class BinomialTree final
     {
-        explicit node(const T& key) : m_degree{0}, m_parent{nullptr}, m_child{nullptr}, m_sibling{nullptr}, m_key{key} {}  
+        public:
+            constexpr BinomialTree(){}
+            ~BinomialTree(){}
+        private:
+            struct node
+            {
+                constexpr explicit node(const T& key) : m_parent{nullptr}, m_child{nullptr}, m_sibling{nullptr}, m_key{key}, m_degree{0}  {}  
 
-        size_t m_degree;
-        node*  m_parent;
-        node*  m_child;
-        node*  m_sibling;
-        T      m_key;
+                node*  m_parent;
+                node*  m_child;
+                node*  m_sibling;
+                T      m_key;
+                size_t m_degree;
+            };
+            std::vector<node*> m_v;
     };
+
     constexpr size_t f(const size_t& n)
     {
-        return n;
     }
 }
 
 int main()
 {
-
+    std::list<ASD::BinomialTree<>> binomial_heap;
+    std::cout << sizeof(ASD::BinomialTree<>) << std::endl;
 }
+
