c318780962bb2d7d5ffd60b1c71372a9d607f5e6
tpalayda <taras.palayda@gmail.com>
2018-05-04 18:05:18 +0200

    some thoughts

--- Exercises/Lista7/t2.cpp
+++ Exercises/Lista7/t2.cpp
@@ -5,25 +5,54 @@
 namespace ASD
 {
     template<typename T = int,size_t size=100>
-    class BinomialTree final
+    class BinomialHeap final
     {
         public:
-            constexpr BinomialTree(){}
-            ~BinomialTree(){}
-        private:
-            struct node
+            constexpr BinomialHeap() {}
+            void insert();
+            void print();
+            void find();
+            T get_max();
+            void make_union();
+            void merge();
+            ~BinomialHeap()
             {
-                constexpr explicit node(const T& key) : m_parent{nullptr}, m_child{nullptr}, m_sibling{nullptr}, m_key{key}, m_degree{0}  {}  
-
-                node*  m_parent;
-                node*  m_child;
-                node*  m_sibling;
-                T      m_key;
-                size_t m_degree;
+                for(auto& m_tree : m_trees)
+                    if(m_tree)
+                        delete m_tree;
+            }
+        private:
+            class BinomialTree final
+            {
+                private:
+                    struct node; // for further purposes
+                public:
+                    constexpr BinomialTree() : m_nodes(10) {}
+                    node* createNode(const T& key)
+                    {
+                        m_nodes.emplace_back(new node(key));
+                    }
+                    ~BinomialTree()
+                    {
+                        for(auto& m_node : m_nodes)
+                            if(m_node)
+                                delete m_node;
+                    }
+                private:
+                    struct node
+                    {
+                        constexpr explicit node(const T& key) : m_parent{nullptr}, m_child{nullptr}, m_sibling{nullptr}, m_key{key}, m_degree{0} {}  
+                        node*  m_parent;
+                        node*  m_child;
+                        node*  m_sibling;
+                        T      m_key;
+                        size_t m_degree;
+                    };
+                    std::vector<node*> m_nodes;
             };
-            std::vector<node*> m_v;
+            std::list<BinomialTree*> m_trees;
     };
-
+    //number of nodes n in binomial tree  - 2^n
     constexpr size_t f(const size_t& n)
     {
     }
@@ -31,7 +60,6 @@ namespace ASD
 
 int main()
 {
-    std::list<ASD::BinomialTree<>> binomial_heap;
-    std::cout << sizeof(ASD::BinomialTree<>) << std::endl;
+    ASD::BinomialHeap<> heap;
 }
 
